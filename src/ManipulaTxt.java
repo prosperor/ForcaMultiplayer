dd3d17966f3fa86a2a363fd90a78a8caeb411260
FabioFernandesCosta <fabio.costa@edu.unifil.br>
2020-11-22 02:20:59 -0300

    ManipulaTxt implementado [R0.1]
    ler issue para mais detalhes: https://github.com/prosperor/ForcaMultiplayer/issues/7

---
+++ src/ManipulaTxt.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ManipulaTxt {
+    String palavra;
+    int iLinhas = -1;
+
+    public ManipulaTxt(){
+        try {
+            FileReader dicio = new FileReader("src/dicio.txt");
+            BufferedReader lerArq = new BufferedReader(dicio);
+            palavra = lerArq.readLine();
+            while (palavra != null){
+                palavra = lerArq.readLine();
+                iLinhas++;
+            }
+
+            dicio.close();
+        }catch (IOException e){
+            System.err.printf("Erro na abertura do arquivo: %s.\n", e.getMessage());
+        }
+
+    }
+
+    public void setPalavra(){
+        try {
+            palavra = Files.readAllLines(Paths.get("src/dicio.txt")).get((int)(Math.random() *  iLinhas));
+        }catch (IOException e){
+            System.err.printf("Erro na abertura do arquivo: %s.\n", e.getMessage());
+        }
+    }
+
+    public String getPalavra(){
+        return palavra;
+    }
+
+    public String getPalavra(boolean bin){
+        setPalavra();
+        return palavra;
+    }
+}
